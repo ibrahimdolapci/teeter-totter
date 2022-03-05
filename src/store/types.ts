76107bbf2b2f9aab50ba59501dd5479cde94b1d8
Ibrahim DOLAPCI <ibrahim@cofeapp.com>
2022-03-05 12:49:45 +0300

    initialize repository

---
+++ src/store/types.ts
@@ -0,0 +1,11 @@
+import {store} from "./index";
+import {Action, ThunkAction} from "@reduxjs/toolkit";
+
+export type AppDispatch = typeof store.dispatch;
+export type RootState = ReturnType<typeof store.getState>;
+export type AppThunk<ReturnType = void> = ThunkAction<
+    ReturnType,
+    RootState,
+    unknown,
+    Action<string>
+    >;
